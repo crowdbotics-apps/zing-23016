9bae2cced82c14a85e0f6eb52dc5cc842ac4f18b
Crowdbotics <team@crowdbotics.com>
2020-11-27 16:29:04 +0000

    Initial commit.

---
+++ ios/zing_23016Tests/zing_23016Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface zing_23016 : XCTestCase
+
+@end
+
+@implementation zing_23016
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
